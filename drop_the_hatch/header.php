9eb970bb4a5293981ab2d2a3b9c21a4b24cffdd7
sean hatch <hatch.sean@yahoo.com>
2020-04-21 13:33:02 -0400

    added loop functionality to the site
    Set the side GUI to utilize the loop function. Currently has CSS to hide the second and third posts.

--- drop_the_hatch/header.php
+++ drop_the_hatch/header.php
@@ -6,15 +6,14 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <?php wp_head();?>
 </head>
-<body>
+<body <?php body_class(); ?>>
 <header class="main-navbar">
     <div id="logoContainer">
-      <img src="<?php echo get_template_directory_uri();?>/images/H_Logo.svg" alt="logo" class="logosvg"></img>
+      <a href="<?php echo site_url();?>"><img src="<?php echo get_template_directory_uri();?>/images/H_Logo.svg" alt="logo" class="logosvg"></img></a>
     </div>
     <nav>
       <ul>
         <li><a href="<?php echo site_url('/photography');?>">Photography</a></li>
-        <li><a href="<?php echo site_url('/videography');?>">Videography</a></li>
         <li><a href="<?php echo site_url('/design');?>">Design</a></li>
         <li><a href="<?php echo site_url('/about');?>">About</a></li>
         <li><a href="<?php echo site_url('/blog');?>">Blog</a></li>
--- drop_the_hatch/index.php
+++ drop_the_hatch/index.php
@@ -1,15 +1,22 @@
 <?php get_header(); ?>
+<div class="main-content-container">
+        <div class="main-content">
+            <?php
+      // TO SHOW THE PAGE CONTENTS
+      while ( have_posts() ) : the_post(); ?>
+            <!--Because the_content() works only inside a WP Loop -->
+            <div>
+                <?php the_content(); ?>
+                <!-- Page Content -->
 
-<?php
-while (have_posts()) {
-    the_post();
-    ?>
-    <a href="<?php the_permalink();?>">
-        <h3><?php the_title();?></h3>
-    </a>
-    <?php the_excerpt();?>
-<?php
-}
-?>
+            </div><!-- .entry-content-page -->
+
+            <?php
+      endwhile; //resetting the page loop
+      wp_reset_query(); //resetting the page query
+      ?>
+            <p>index
+            </p>
+        </div>
 
 <?php get_footer(); ?>
